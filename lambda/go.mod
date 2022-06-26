9a97c496405131acfdaca9fbf62cb98fbcb1388e
Udomomo <udomomo@gmail.com>
2022-06-26 17:37:27 +0900

    update lambda function to use line-bot-sdk

--- lambda/go.mod
+++ lambda/go.mod
@@ -2,4 +2,7 @@ module github.com/Udomomo/honey_island_bot
 
 go 1.17
 
-require github.com/aws/aws-lambda-go v1.32.0
+require (
+	github.com/aws/aws-lambda-go v1.32.0
+	github.com/line/line-bot-sdk-go/v7 v7.16.0
+)
--- lambda/main.go
+++ lambda/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
 type V1Request struct {
@@ -18,38 +24,71 @@ type V1Response struct {
 	Message string `json:"message:"`
 }
 
-func ExtractRequestBody(input string) (*V1Request, error) {
-	var req V1Request
-	err := json.Unmarshal([]byte(input), &req)
+func ValidateSignature(channelSecret, signature string, body []byte) bool {
+	decoded, err := base64.StdEncoding.DecodeString(signature)
 	if err != nil {
-		return nil, err
+		log.Println(err)
+		return false
 	}
-	return &req, nil
+	hash := hmac.New(sha256.New, []byte(channelSecret))
+	hash.Write(body)
+	return hmac.Equal(decoded, hash.Sum(nil))
 }
 
-func HandleLambdaEvent(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	body, err := ExtractRequestBody(req.Body)
-
+func HandleLambdaRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	bot, err := linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("ACCESS_TOKEN"))
 	if err != nil {
+		log.Print(err)
 		return events.APIGatewayProxyResponse{
 			Body:       err.Error(),
 			StatusCode: 500,
 		}, err
 	}
 
-	if body.IsSolved {
+	if !ValidateSignature(os.Getenv("CHANNEL_SECRET"), req.Headers["X-Line-Signature"], []byte(req.Body)) {
+		log.Print(err)
 		return events.APIGatewayProxyResponse{
-			Body:       fmt.Sprintf("%dに正解しました", body.PuzzleId),
-			StatusCode: 200,
+			StatusCode: http.StatusBadRequest,
+			Body:       fmt.Sprintf(`{"message":"%s"}`+"\n", linebot.ErrInvalidSignature.Error()),
 		}, nil
 	}
 
+	puzzleResults := &struct {
+		Destination string           `json:"destination"`
+		Events      []*linebot.Event `json:"events"`
+	}{}
+	if err := json.Unmarshal([]byte(req.Body), puzzleResults); err != nil {
+		log.Print(err)
+		return events.APIGatewayProxyResponse{
+			Body:       err.Error(),
+			StatusCode: 500,
+		}, err
+	}
+
+	for _, puzzleResult := range puzzleResults.Events {
+		switch m := puzzleResult.Message.(type) {
+		case *linebot.TextMessage:
+			if _, err = bot.ReplyMessage(puzzleResult.ReplyToken, linebot.NewTextMessage(m.Text)).Do(); err != nil {
+				log.Print(err)
+				return events.APIGatewayProxyResponse{
+					StatusCode: http.StatusBadRequest,
+					Body:       fmt.Sprintf(`{"message":"%s"}`+"\n", http.StatusText(http.StatusBadRequest)),
+				}, nil
+			}
+		default:
+			log.Print(err)
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusBadRequest,
+				Body:       fmt.Sprintf(`{"message":"%s"}`+"\n", http.StatusText(http.StatusBadRequest)),
+			}, nil
+		}
+	}
+
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("%dをスキップしました", body.PuzzleId),
 		StatusCode: 200,
 	}, nil
 }
 
 func main() {
-	lambda.Start(HandleLambdaEvent)
+	lambda.Start(HandleLambdaRequest)
 }
